b627917868208fe4388a34228ca01f0851a2f64a
lunaro <pavelike@gmail.com>
2025-09-18 18:42:52 +0300

    WIP: work on multiplayer

---
+++ scenes/player.gd
@@ -0,0 +1,21 @@
+class_name Player extends RigidBody3D
+
+
+
+@onready var player_camera : Camera3D = %PlayerCamera
+
+
+func _ready():
+	assert(player_camera)
+	# Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	player_camera.current = is_multiplayer_authority()
+	pass
+
+# @rpc("call_local")
+func setup_multiplayer(id: int):
+	set_multiplayer_authority(id)
+	pass
+
+func _enter_tree() -> void:
+	set_multiplayer_authority(name.to_int())
+	pass
