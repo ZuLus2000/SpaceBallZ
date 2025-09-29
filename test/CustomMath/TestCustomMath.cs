3828266e8e244db5c9582c1fbb13bbb577f8098d
lunaro <pavelike@gmail.com>
2025-09-29 14:35:59 +0300

    add first test

---
+++ test/CustomMath/TestCustomMath.cs
@@ -0,0 +1,9 @@
+namespace Tests;
+
+using Godot;
+using System;
+using GdUnit4;
+
+public partial class TestCustomMath : Node
+{
+}
---
+++ test/CustomMath/test_custom_math.gd
@@ -0,0 +1,11 @@
+class_name TestCustomMath
+extends GdUnitTestSuite
+
+# @warning_ignore('unused_parameter')
+
+
+
+func test_in_range() -> void:
+	var is_in_range_value : bool
+	is_in_range_value = CustomMath.is_in_range(1, 0, 2,)
+	assert_bool(is_in_range_value).is_true()
