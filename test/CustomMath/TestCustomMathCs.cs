261fef3846ec6cf3900c80e7738c83842121d1fd
lunaro <pavelike@gmail.com>
2025-10-10 12:30:56 +0300

    test(CustomMath): add tests for CustomMath class

--- test/CustomMath/TestCustomMathCs.cs
+++ test/CustomMath/TestCustomMathCs.cs
@@ -1,8 +1,9 @@
 
 using GdUnit4;
 using static GdUnit4.Assertions;
+using System.Collections.Generic;
 
-// using Godot;
+using Godot.Collections;
 
 using static CustomMath.CustomMath;
 
@@ -11,15 +12,32 @@ using static CustomMath.CustomMath;
 [TestSuite]
 public class TestCustomMathCs
 {
-	// private GodotObject CustomMath = (GodotObject)GD.Load<GDScript>("res://components/math.gd").New();
 	
 
-	[TestCase]
-	public void InRange() {
-		bool inRange;
-		inRange = IsInRange(1, 0, 2);
-		AssertBool(inRange).IsTrue();
+	[TestCase(1,0,2, true)]
+	[TestCase(0,1,2, false)]
+	[TestCase(1,int.MinValue,int.MaxValue, true)]
+	[TestCase(int.MinValue,1,int.MaxValue, false)]
+	[TestCase(int.MaxValue,int.MinValue,1, false)]
+	public void TestInRange(int testValue, int rangeStart, int rangeEnd, bool expect ) {
+		AssertThat(IsInRange(testValue, rangeStart, rangeEnd)).Equals(expect);
 	}
+		HashSet<double> hash = new HashSet<double> {0.0, 2.0};
+
+	[TestCase(1,0,2, new double[2] {1, 2})]
+	[TestCase(0,1,2, new double[2] {0, 0})]
+	[TestCase(0,double.MinValue,double.MaxValue, new double[2] {double.MinValue, double.MaxValue})]
+	public void TestUnclamp(double testValue, double rangeStart, double rangeEnd, double[] expect)
+	{
+		double result = Unclamp(testValue, rangeStart, rangeEnd);
+		bool isInExpect = false;
+		
+		foreach (double value in expect)
+		    if (result == value) { isInExpect = true; break; }
+
+		AssertBool(isInExpect).IsTrue();
+	}
+
 
 }
 
