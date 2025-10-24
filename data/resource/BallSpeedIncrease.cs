88ad3cd3e799246fb2a86a853dd314ca10c15d0f
lunaro-4 <pavelike@gmail.com>
2025-10-24 20:56:46 +0300

    feat(Modifiers): add modifier framework
    - Add IBuffable interface
    - Make Ball IBuffable
    - Add abstract modifier and BallSpeedIncrease resources

---
+++ data/resource/BallSpeedIncrease.cs
@@ -0,0 +1,37 @@
+using Godot;
+using System;
+
+namespace SpaceBallZ
+{
+    public partial class BallSpeedIncrease : BallModifier
+    {
+        protected override String _name { get { return "BallSpeedIncrease"; } }
+
+        [Export]
+        protected override int _duration { get; set; }
+
+        [Export]
+        private float _multiplySpeedValue;
+
+        public BallSpeedIncrease(int duration, float increaseValue)
+        {
+            _duration = duration;
+            _multiplySpeedValue = increaseValue;
+        }
+
+
+        public override void MakeEffect(Node applicant)
+        {
+            Ball ball = applicant as Ball;
+            ball.Speed = ball.InitialSpeed * _multiplySpeedValue;
+        }
+
+        public override void Clean(IBuffable applicant)
+        {
+            Ball ball = applicant as Ball;
+            ball.Speed = ball.InitialSpeed;
+        }
+
+    }
+}
+
--- scenes/Ball.cs
+++ scenes/Ball.cs
@@ -1,13 +1,35 @@
 
 using Godot;
 using static CustomMath.CustomMath;
+using System.Collections.Generic;
 
 namespace SpaceBallZ
 {
-    public partial class Ball : RigidBody3D
+    public partial class Ball : RigidBody3D, IBuffable
     {
-        public float Speed = 0f;
+        public float InitialSpeed { get; private set; }
+        public float Speed { get; set; }
         public double StraightAngleMargin = 0.1;
+
+        public HashSet<BallModifier> ActiveBuffs { get; private set; }
+
+
+        public Ball New(float initialSpeed, double straightAngleMargin)
+        {
+            InitialSpeed = initialSpeed;
+            Speed = InitialSpeed;
+            StraightAngleMargin = straightAngleMargin;
+            return this;
+        }
+
+        void IBuffable.UpdateBuffs()
+        {
+            foreach (BallModifier buff in ActiveBuffs)
+            {
+                continue;
+            }
+        }
+
 
         public override void _PhysicsProcess(double delta)
         {
